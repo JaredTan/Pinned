dabe257c7e1da024ec0aeaa95a77665ce5a4c57d
JaredTan <jared.e.tan@gmail.com>
2017-07-19 01:39:05 -0700

    nav bar start

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,21 +3,17 @@ import { Provider } from 'react-redux';
 import { Route, Switch, Link} from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session/session_form_container';
-import { AuthRoute } from '../util/route_util';
+import NavBarContainer from './nav/nav_bar_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Image from './image/cloud';
 
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/" className="header">
-        <h1>Pinned</h1>
-      </Link>
-    </header>
-    <GreetingContainer/>
     <Switch>
       <AuthRoute path="/signup" component={SessionFormContainer} />
       <AuthRoute path="/login" component={SessionFormContainer} />
+      <ProtectedRoute path="/" component={NavBarContainer} />
       <Route path="/images" component={Image} />
     </Switch>
   </div>
