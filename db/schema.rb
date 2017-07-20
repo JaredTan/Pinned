ed05e94026f1a938a78b64fff7cef82554c4aecc
JaredTan <jared.e.tan@gmail.com>
2017-07-19 21:48:13 -0700

    font change

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170719165043) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_digest", null: false
